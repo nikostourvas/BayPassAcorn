3f0282cf979e88219a3c2bee2ef5ed5d547ce0d5
nikostourvas <nikostourvas@gmail.com>
2024-08-30 18:43:05 +0300

    separate diagnostics scripts for different models

---
+++ model_diagnostics_covariate.R
@@ -0,0 +1,23 @@
+# STD IS model diagnostics
+snp.res=read.table(snakemake@input[[1]],h=T) 
+
+pdf(snakemake@output[[1]],width=10,height=10)
+layout(matrix(1:3,3,1)) 
+plot(snp.res$BF.dB.,xlab="SNP",ylab="BFis (in dB)") 
+plot(snp.res$eBPis,xlab="SNP",ylab="eBPis") 
+plot(snp.res$Beta_is,xlab="SNP",ylab=expression(beta~"coefficient"))
+dev.off()
+
+# Genetic offset and RONA
+# go.is = compute_genetic_offset(beta.coef=NULL,
+#                                 regfile=snp.res,
+#                                 covfile=snakemake@params[[1]], 
+#                                 newenv='ADDRELEVANTFILE',refenv=NULL,
+#                                 scalecov=TRUE,candidate.snp=NULL, 
+#                                 compute.rona=TRUE)
+
+# write.table(go.is$go,snakemake@output[[3]],quote=F,sep=",",row.names=F)
+# write.table(go.is$BtBeigenvalues,snakemake@output[[4]],quote=F,sep=",",row.names=F)
+# write.table(go.is$BtBeigenvectors,snakemake@output[[5]],quote=F,sep=",",row.names=F)
+# write.table(go.is$covimp,snakemake@output[[6]],quote=F,sep=",",row.names=F)
+# write.table(go.is$RONA,snakemake@output[[7]],quote=F,sep=",",row.names=F)
