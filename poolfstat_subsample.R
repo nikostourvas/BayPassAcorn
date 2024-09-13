779537ef0c6decff4bab0ba198fc8a39e357097c
nikostourvas <nikostourvas@gmail.com>
2024-09-13 20:07:53 +0300

    minor refactoring to list the inputs properly instead of treating them as params

--- poolfstat_subsample.R
+++ poolfstat_subsample.R
@@ -2,8 +2,8 @@
 library(poolfstat)
 
 # generate a pooldata object
-psizes <- as.numeric(read.table(snakemake@params[[1]], header = FALSE, sep = " "))
-pnames <- as.character(read.table(snakemake@params[[2]], header = FALSE, sep = " "))
+psizes <- as.numeric(read.table(snakemake@input[[2]], header = FALSE, sep = " "))
+pnames <- as.character(read.table(snakemake@input[[3]], header = FALSE, sep = " "))
 
 poolreadcount <- vcf2pooldata(vcf.file = snakemake@input[[1]], 
                               poolsizes = psizes, poolnames = pnames)
@@ -13,6 +13,6 @@ poolreadcount
 # convert the pooldata object to BayPass input files (i.e. genobaypass, snpdet, poolsize)
 # and create multiple subsets that cover the whole data set
 pooldata2genobaypass(poolreadcount, writing.dir = "results/subsets", 
-                     prefix = snakemake@params[[3]],
-                     subsamplesize = nrow(poolreadcount@readcoverage)/snakemake@params[[4]],
+                     prefix = snakemake@params[[1]],
+                     subsamplesize = nrow(poolreadcount@readcoverage)/snakemake@params[[2]],
                      subsamplingmethod = "thinning")
