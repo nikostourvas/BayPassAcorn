8aa4463c0b35c7ff4d1bf74b79555b7a1fc3d065
nikostourvas <nikostourvas@gmail.com>
2024-09-13 20:09:49 +0300

    eliminate redanduncies

--- concatenate_res.R
+++ concatenate_res.R
@@ -1,33 +1,12 @@
 source("baypass_utils.R")
-# all.res.core = concatenate_res(
-#                         anaprefix = snakemake@params[[1]],
-#                         nsubsets=20,
-#                         snpdet_prefix = snakemake@params[[4]], 
-#                         retrieve_pi_xtx=TRUE,
-#                         retrieve_bfis=FALSE,
-#                         retrieve_c2=FALSE)
 
-# write.table(all.res.core, snakemake@output[[1]], sep = ",", 
-#             row.names = FALSE, quote = FALSE)
-
-all.res.std = concatenate_res(
+all.res = concatenate_res(
                         anaprefix = snakemake@params[[1]],
-                        nsubsets=snakemake@params[[3]],
-                        snpdet_prefix = snakemake@params[[4]], 
+                        nsubsets=snakemake@params[[2]],
+                        snpdet_prefix = snakemake@params[[3]], 
                         retrieve_pi_xtx=TRUE,
                         retrieve_bfis=TRUE,
-                        retrieve_c2=FALSE)
+                        retrieve_c2=snakemake@params[[4]])
 
-write.table(all.res.std, snakemake@output[[1]], sep = ",",
-            row.names = FALSE, quote = FALSE)
-
-all.res.contrast = concatenate_res(
-                        anaprefix = snakemake@params[[2]],
-                        nsubsets=snakemake@params[[3]],
-                        snpdet_prefix = snakemake@params[[4]], 
-                        retrieve_pi_xtx=TRUE,
-                        retrieve_bfis=TRUE,
-                        retrieve_c2=TRUE)
-
-write.table(all.res.contrast, snakemake@output[[2]], sep = ",", 
+write.table(all.res, snakemake@output[[2]], sep = ",", 
             row.names = FALSE, quote = FALSE)
