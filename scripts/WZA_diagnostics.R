2411b07dc35c41a4f0115cdc95d1bc69a724ee23
nikostourvas <nikostourvas@gmail.com>
2024-12-14 02:13:49 +0200

    add gif correction

--- scripts/WZA_diagnostics.R
+++ scripts/WZA_diagnostics.R
@@ -6,10 +6,6 @@ library(tidyr)
 
 FDR_LEVEL=snakemake@params[[2]]
 
-############################
-# Manhattan plots
-############################
-
 envfactors = readLines(snakemake@input[[1]])
 envfactors = factor(envfactors, levels=envfactors)
 print(envfactors)
@@ -19,19 +15,35 @@ for (i in envfactors){
   WZA_res[[i]] = fread(paste0(snakemake@params[[1]], i, "_WZA_output.csv"))
 }
 
+# Calculate the genomic inflation factor (GIF) based on all the Z-scores.
+gif = list()
+for (i in envfactors){
+  gif[[i]] = median((WZA_res[[i]]$Z)^2)/(qchisq(0.5, df = 1, lower.tail = FALSE)) 
+}
+# Save the GIFs to a file
+gif_table = reshape2::melt(gif)
+write.table(gif_table, file=paste0(snakemake@params[[1]], "GIF.csv"), sep=",", row.names=FALSE, quote=FALSE)
+
+# Calculate the gif corrected p-values and q-values
+print("Calculating p-values")
+for (i in envfactors){
+  WZA_res[[i]]$Z_pVal_gif_adj = pchisq(WZA_res[[i]]$Z^2/gif[[i]], df = 1, lower.tail = FALSE)
+}
+
+print("Calculating q-values")
 # FDR correction
 # for some reason, while testing with a toy dataset, WZA produced few NAs
 # Until we investigate further, I replace those NAs with 1
-WZA_res = lapply(WZA_res, function(x) {x$Z_pVal[is.na(x$Z_pVal)] = 1; return(x)})
-WZA_res = lapply(WZA_res, function(x) {x$qvalue = qvalue(x$Z_pVal, fdr.level=FDR_LEVEL)$significant; return(x)})
-WZA_res = lapply(WZA_res, function(x) {x$qvalue0.001 = qvalue(x$Z_pVal, fdr.level=0.001)$significant; return(x)})
+#WZA_res = lapply(WZA_res, function(x) {x$Z_pVal_gif_adj[is.na(x$Z_pVal)] = 1; return(x)})
+WZA_res = lapply(WZA_res, function(x) {x$qvalue = qvalue(x$Z_pVal_gif_adj, fdr.level=FDR_LEVEL)$significant; return(x)})
+WZA_res = lapply(WZA_res, function(x) {x$qvalue0.001 = qvalue(x$Z_pVal_gif_adj, fdr.level=0.001)$significant; return(x)})
 # export each table to a file
 for (i in envfactors){
   write.table(WZA_res[[i]], file=paste0(snakemake@params[[1]], i, "_WZA_output_fdr.csv"), 
               sep=",", row.names=FALSE, quote=FALSE)
 }
 
-WZA_res = lapply(WZA_res, function(x) x[ ,c(1,2,6,7,8,9)]) # keep only the columns we need for plotting
+WZA_res = lapply(WZA_res, function(x) x[ ,c(1,2,6,7,8,9,10)]) # keep only the columns we need for plotting
 WZA_res = reshape2::melt(WZA_res, id.vars = c("index", "SNPs", "POS"))
 # Make the dataset wide. Spread the "variable" column into multiple columns
 WZA_res = spread(WZA_res, variable, value)
@@ -43,13 +55,18 @@ WZA_res$index = as.factor(gsub("Sc0000", "", WZA_res$index)) # remove prefix fro
 # remove from rows of the column WZA_res$index the suffix "_window_" and anything after that
 WZA_res$index = as.factor(gsub("_window_.*", "", WZA_res$index))
 
-colnames(WZA_res) = c("CHR", "SNPs", "POS", "envfactor", "value", "qvalue", "qvalue0.001")
+colnames(WZA_res) = c("CHR", "SNPs", "POS", "envfactor", "pvalue", "gif_cor_pvalue", "qvalue", "qvalue0.001")
+write.table(WZA_res, file=paste0(snakemake@params[[1]], "WZA_test.csv"), sep=",", row.names=FALSE, quote=FALSE)
 
 WZA_res$CHR = as.factor(gsub("^0", "", WZA_res$CHR)) # replace 01, 02 etc with 1, 2 etc
 WZA_res$CHR = factor(WZA_res$CHR, levels=unique(WZA_res$CHR))
 
+############################
+# Manhattan plots
+############################
+
 # add a column with the calculated -log10(p-value)
-WZA_res$score = -log10(WZA_res$value)
+WZA_res$score = -log10(WZA_res$gif_cor_pvalue)
 
 p <- ggplot(WZA_res, aes(x=POS, y=score, color=CHR)) + 
   geom_point(alpha=1, size=0.8) +
@@ -89,16 +106,48 @@ for (env in envfactors) {
   data_subset <- subset(WZA_res, envfactor == env)
   
   # Plot the p-value distribution
-  hist(data_subset$value, col = "grey", main = paste("P-value Distribution -", env), xlab = "P-values")
+  hist(data_subset$pvalue, col = "grey", main = paste("P-value Distribution -", env), xlab = "P-values")
   
   # Plot the QQ-plot
-  observed <- -log10(sort(data_subset$value))
+  observed <- -log10(sort(data_subset$pvalue))
   expected <- -log10(ppoints(length(observed)))
   plot(expected, observed, xlab = "Expected -log10(P)", ylab = "Observed -log10(P)", main = paste("QQ-Plot -", env), pch = 19, cex = 1)
   abline(0, 1)
   
   # Add median p-value to the QQ-plot
-  median_pvalue <- median(data_subset$value)
+  median_pvalue <- median(data_subset$pvalue)
+  points(-log10(median_pvalue), -log10(median_pvalue), col = "red", pch = 17, cex = 2)
+  legend("bottomright", legend = paste("Median P = ", round(median_pvalue, 4)), col = "red", pch = 17, cex = 1)
+  
+  # Close the PNG device
+  dev.off()
+}
+
+# Loop over each envfactor
+for (env in envfactors) {
+  # Set up the file name for the PNG
+  png_filename <- paste0(snakemake@params[[1]], "Pvalue_and_QQ_Plots_(GIF corrected)", env, ".png")
+  
+  # Open the PNG device
+  png(filename = png_filename, width = 1050, height = 750, units = "px")
+  
+  # Set up the plotting area
+  par(mfrow = c(1, 2), mar = c(5, 5, 4, 1))
+  
+  # Subset data for the current envfactor
+  data_subset <- subset(WZA_res, envfactor == env)
+  
+  # Plot the p-value distribution
+  hist(data_subset$gif_cor_pvalue, col = "grey", main = paste("P-value Distribution -", env), xlab = "P-values")
+  
+  # Plot the QQ-plot
+  observed <- -log10(sort(data_subset$gif_cor_pvalue))
+  expected <- -log10(ppoints(length(observed)))
+  plot(expected, observed, xlab = "Expected -log10(P)", ylab = "Observed -log10(P)", main = paste("QQ-Plot -", env), pch = 19, cex = 1)
+  abline(0, 1)
+  
+  # Add median p-value to the QQ-plot
+  median_pvalue <- median(data_subset$gif_cor_pvalue)
   points(-log10(median_pvalue), -log10(median_pvalue), col = "red", pch = 17, cex = 2)
   legend("bottomright", legend = paste("Median P = ", round(median_pvalue, 4)), col = "red", pch = 17, cex = 1)
   
